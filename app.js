46b79f3f0b04f9179639a761572fd96f53cee29f
Imrymimran <imrantayyab30@gmail.com>
2023-12-09 11:55:06 +0500

    completed

---
+++ app.js
@@ -0,0 +1,69 @@
+// alert("This is a message");
+
+// var personName = prompt("Enter your name" , "Guest");
+// console.log(personName);
+
+// var marks = 86;
+// console.log(marks);
+// alert(marks);
+
+
+// var a = 10
+// var b = 20
+// var c = 40
+// var d = 60
+// console.log( (a+b)-c*d)
+
+
+// var a = 20;
+// var b = a++;
+
+// console.log(b, "B value")
+// console.log(a, "A value")
+
+
+
+// var a = 10;
+// var b = 20;
+// // var b = "20";
+// var c = a+b;
+
+// console.log(c);
+
+// var age = 18;
+// var studentCard = true:
+// if(age >= 18){
+//     console.log("condition true");
+// }else{
+//     if(studentCard == true){
+//         console.log("Allow on student Card")
+//     }
+// }
+
+
+var a = "A";
+var b = "B";
+
+var arr = ["A","B","C","D","E"]
+// console.log(arr);
+// arr[2] = 12
+
+// console.log(arr);
+
+
+// arr[5] = "D";
+
+// console.log(arr);
+
+// arr.push ("D");
+// arr.pop();
+// arr.shift();
+// arr.unshift("1");
+// console.log (arr);
+// var a = arr.slice (1,4);
+// console.log(a);
+
+var a = arr.splice(2,2, "1", "2", "3", "4");
+console.log(a);
+console.log(arr);
+
